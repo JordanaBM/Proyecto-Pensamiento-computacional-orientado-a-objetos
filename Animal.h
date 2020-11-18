93ab1478fde15fe45f401577d75995a4275ec96c
JordanaBM <69861226+JordanaBM@users.noreply.github.com>
2020-11-18 09:35:43 -0600

    Clase padre Animal con herencia.
    Clases hijas Mamifero y Oviparo

--- Animal.h
+++ Animal.h
@@ -1,13 +1,12 @@
 /*
  * Clase Animal
-*Permite la creación de carnets de nuevos animales
+ *Permite la creación de objetos tipo animal
  * que ingresan al albergue
  */
  
 #include <iostream>
 #include <string>
 using namespace std;
-
 
 
 class Animal{
@@ -19,15 +18,16 @@ class Animal{
 		string color;
 		int edad; //Edad en meses
 		string fecha_e; //Fecha de entrada al albergue
-		Animal *sig;
+		
 		
 	public: //Métodos
 		//Constructores
 		Animal(): tipo(""),nombre(""), raza(""),genero(""),color(""),edad(), fecha_e(){}; //Por default
-		Animal(string tip,string nom, string raz,string gen,string col,int ed, string f_e): tipo(tip),nombre(nom),raza(raz),genero(gen),color(col),edad(ed),fecha_e(f_e){};
+		Animal(string tip,string nom, string raz,string gen,string col,int ed, string f_e): 
+		tipo(tip),nombre(nom),raza(raz),genero(gen),color(col),edad(ed),fecha_e(f_e){};
 		
 		
-		void print(); // Imprime los datos del  animal
+		virtual void print(); // Imprime los datos del animal, usa polimorfismo
 		
 		string get_tipo();//Funciones get
 		string get_nombre(); 
@@ -36,7 +36,7 @@ class Animal{
 		string get_color();
 		int get_edad();
 		string get_fecha_e();
-		Animal* get_sig(); //Función para obtener la posición siguiente del apuntador
+		
 		
 		void set_tipo(string); //Funciones set
 		void set_nombre(string); 
@@ -44,10 +44,8 @@ class Animal{
 		void set_genero(string);
 		void set_color(string);
 		void set_edad(int);
-		void set_fecha_e(string);
+		void set_fecha_e(string);	
 		
-		//Cambio de posición apuntador
-		void setSig(Animal *sig);
 };
 
 //getters
@@ -71,9 +69,6 @@ int Animal:: get_edad(){
 }
 string Animal:: get_fecha_e(){
 	return fecha_e;
-}
-Animal* Animal:: get_sig(){
-	return sig;
 }
 
 
@@ -105,7 +100,7 @@ void Animal :: print(){
 	cout<<"Tipo: "<<tipo<<endl;
 	cout<<"Nombre: "<<nombre<<endl;
 	cout<<"Raza: "<<raza<<endl;
-	cout<<"Género: "<<genero<<endl;
+	cout<<"Genero: "<<genero<<endl;
 	cout<<"Color: "<<color<<endl;
 	cout<<"Edad: "<<edad<<" meses"<<endl;
 	cout<<"Fecha de entrada: "<<fecha_e<<endl;
@@ -113,74 +108,94 @@ void Animal :: print(){
 		cout<<"Estado: No adoptable\n"; // No se puede adoptar
 	else
 		cout<<"Estado: Adoptable\n"; // Si es menor a los 7 años
-}
-	
-void Animal :: setSig(Animal* sig){
-	this -> sig = sig; // El this -> sig es mi atributo de clase persona y toma el valor siguiente (parametro) del apuntador
+		
 }
 
-class Lista_c{
-	
+class Mamifero : public Animal{ //HERENCIA CLASE ANIMAL
 	private:
-	Animal *principio; //Apuntador principio
-	public:
-	void agregar(void); //Método agregar
-	void mostrar(void); //Método mostrar
+	string pelo;
+	string hijos;
 	
-	Lista_c(); //Constructor 
+	public:
+	Mamifero(string tip,string nom, string raz,string gen,string col,int ed, string f_e, string pel,string hij): 
+		Animal(tip,nom,raz,gen,col,ed,f_e),pelo(pel),hijos(hij){};
+		
+		void print();
+		
+		string get_pelo();//Funciones get
+		string get_hijos(); 
+				
+		
+		void set_pelo(string); //Funciones set
+		void set_hijos(string); 
+	
 };
 
-Lista_c :: Lista_c(){ //No hay nada al principio del constructor
-	principio = NULL; 
+
+//getters
+string Mamifero::get_pelo(){
+	return pelo;
+}
+string Mamifero:: get_hijos(){
+	return hijos;
 }
 
-void Lista_c :: agregar(void){
-	
-	string tipo;
-	string nombre;
-	string raza;
-	string genero;
-	string color;
-	int edad; //Edad en meses
-	string fecha_e; //Fecha de entrada al albergue
-	
-	cout<<"¿Qúe tipo de animal es? ";
-	cin>>tipo;
-	cout<<"¿Cual es su nombre? ";
-	cin>>nombre;
-	cout<<"¿Cual es su raza? ";
-	cin>>raza;
-	cout<<"¿Cual es su genero? ";
-	cin>>genero;
-	cout<<"¿Cual es su edad en meses? ";
-	cin>>edad;
-	cout<<"¿Cual es su color? ";
-	cin>>color;
-	cout<<"¿Cual es su fecha de entrada? ";
-	cin>>fecha_e;
-	
-	
-	Animal *nuevo = new Animal(tipo,nombre,raza,genero,color,edad,fecha_e); //Cambia al apuntador a una posición nueva sin datos
-	
-	if (principio == NULL)
-		principio = nuevo;
-	else
-	{
-		nuevo -> setSig( principio );
-		principio = nuevo; 
-	}
-	
-	
+//setters
+void Mamifero ::set_pelo(string pel){
+	pelo = pel;
 }
 
-void Lista_c :: mostrar(void){
-	for (Animal *aux= principio; aux != NULL; aux = aux -> get_sig()){
-		cout<<"Tipo: "<< aux -> get_tipo()<<endl; //El auxiliar de mi apuntador toma los get de la clase Animal
-		cout<<"Nombre: "<< aux -> get_nombre()<<endl;
-		cout<<"Raza: "<< aux -> get_raza()<<endl;
-		cout<<"Genero: "<< aux -> get_genero()<<endl;
-		cout<<"Edad: "<< aux -> get_edad()<<" meses"<<endl;
-		cout<<"Color: "<<aux -> get_color()<<endl;
-		cout<<"Fecha de entrada: "<<aux -> get_fecha_e()<<endl;
-	}
+void Mamifero ::set_hijos(string hij){
+	hijos = hij;
+}
+
+//Funciones
+void Mamifero :: print(){
+	Animal :: print();
+	cout<<"Su pelaje es: "<<pelo<<endl;
+	cout<<"Puede tener hijos: "<<hijos<<endl;
+}
+
+class Oviparo : public Animal{ //HERENCIA CLASE ANIMAL
+	private:
+	string piel;
+	string huevos;
+	
+	public:
+	Oviparo(string tip,string nom, string raz,string gen,string col,int ed, string f_e, string pie, string huev):
+		Animal(tip,nom,raz,gen,col,ed,f_e),piel(pie),huevos(huev){};
+		
+		void print();
+		
+		string get_piel();//Funciones get
+		string get_huevos(); 
+				
+		
+		void set_piel(string); //Funciones set
+		void set_huevos(string); 
+	
+};
+
+//getters
+string Oviparo::get_piel(){
+	return piel;
+}
+string Oviparo:: get_huevos(){
+	return huevos;
+}
+
+//setters
+void Oviparo ::set_piel(string pie){
+	piel = pie;
+}
+
+void Oviparo::set_huevos (string huev){
+	huevos = huev;
+}
+
+//Funciones
+void Oviparo :: print(){
+	Animal :: print();
+	cout<<"Tipo de piel: "<<piel<<endl;
+	cout<<"Puede poner huevos: "<<huevos<<endl;
 }
