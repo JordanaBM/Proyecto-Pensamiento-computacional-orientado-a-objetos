66c5383d2621dc2b0bc0de1c545af56973c034e4
JordanaBM <69861226+JordanaBM@users.noreply.github.com>
2020-11-18 09:43:38 -0600

    main.cpp
    En la opción 2 "Ver datos" se puede observar los datos de los objetos "perro" y "ave"

--- albergue.cpp
+++ albergue.cpp
@@ -5,58 +5,131 @@
  * A01707434
  */
 
-/**
+/*
  * Programa "Albergue de Animales" permite el registro y muestra
  * de información sobre animales que lleguen a un albergue 
- * Incluye clase animales, personas y donativos
+ * Incluye clase padre animal y herencia en clase mamifero y oviparo
  * Devueleve el carnet de los animales y permite o no darlo en adopción
- * Permite generar donaciones para el cuidado de los animales
- * Resgistra datos de personas con interés de adoptar
+ * Se pueden modificar los datos de los animales
+ * Puede encontrar la mascota de tu preferencia
  */ 
 
 //Bibliotecas
 #include <iostream>   //Para imprimir
 #include "Animal.h" //Clase Animal
-#include "Persona.h" // Clase persona
-#include "Donativo.h" //Clase donativo
+
 
 using namespace std;
 
 int main()
 {
-	// Objeto tipo Animal
+	int opcion,opcion_2;
+	Mamifero perro("Perro","Scooby","Gran Danes","Macho","Cafe",23,"18/11/2020","Corto","No");
+	Oviparo ave("Pajaro","Piolin","Canario","Hembra","Amarillo",3,"18/11/2020","Plumas","Si");
 	
-	cout<<"Ingresa 2 animales\n";
-	Lista_c Animales;
-	
-	for ( int i=0; i<2; i++){
-		Animales.agregar();
-	}
-	Animales.mostrar();
-	
-	
-	// Objeto tipo Persona
-	
-	cout<<"Ingresa 2 donaciones\n";
-	Lista_b Person;
-	
-	for ( int i=0; i<2; i++){
-		Person.agregar();
-	}
-	Person.mostrar();
-	
-	
-	// Objeto tipo Donación
-	
-	cout<<"Ingresa 2 donaciones\n";
-	Lista_a Donar;
-	
-	for ( int i=0; i<2; i++){
-		Donar.agregar();
-	}
-	Donar.mostrar();
-	
-	
-	
-	
+	do //Menú de opciones
+	{
+		cout<<"ALBERGUE DE ANIMALES\n";
+		cout<<"1. Ingresar nuevos datos\n";
+		cout<<"2. Ver datos\n";
+		cout<<"3. Cambiar datos\n";
+		cout<<"4. Encuentra tu mascota\n";
+		cout<<"5. Salir\n";
+		cout<<"\n Digite la opcion que desee: ";
+		cin>>opcion;
+		
+		switch (opcion)
+		{
+			case 1: //Para el registro de nuevos datos
+			cout<<"\nREGISTROS\n\n";
+			cout<<"1. Nuevo registro mamifero\n";
+			cout<<"2. Nuevo registro oviparo\n";
+			cout<<"3. Salir al menu principal\n";
+			cout<<"\n Digite la opcion que desee: ";
+			cin>>opcion_2;
+			
+				switch (opcion_2)
+				{
+				case 1:
+				cout<<"Nuevo registro de mamifero\n";
+				
+				break;
+				
+				case 2: 
+				cout<<"Nuevo registro de oviparo\n";
+				
+				break;
+				
+				case 3:
+				cout<<"Volviendo al menu principal\n";
+				break;
+				}
+				
+			break;
+					
+			case 2: //Para visualizar los datos
+			cout<<"\nDATOS\n\n";
+			cout<<"1. Datos Mamiferos\n";
+			cout<<"2. Datos Oviparos\n";
+			cout<<"3. Salir al menu principal\n";
+			cout<<"\n Digite la opcion que desee: ";
+			cin>>opcion_2;
+			
+				switch (opcion_2)
+				{
+				case 1:
+				cout<<"Mamiferos registrados\n"<<endl;
+				perro.print();
+				cout<<endl;
+				break;
+				
+				case 2: 
+				cout<<"Oviparos registrados\n\n";
+				ave.print();				
+				cout<<endl;
+				break;
+				
+				case 3:
+				cout<<"Volviendo al menu principal\n";
+				break;
+				}
+				
+			break;
+			
+			case 3: //Para modificar los datos
+			cout<<"\nMODIFICACION DE DATOS\n\n";
+			cout<<"1. Cambiar Registro Mamiferos\n";
+			cout<<"2. Cambiar Registro Oviparos\n";
+			cout<<"3. Salir al menu principal\n";
+			cout<<"\n Digite la opcion que desee: ";
+			cin>>opcion_2;
+			
+			switch (opcion_2)
+				{
+				case 1:
+				cout<<"Cambiar registro de Mamiferos\n";
+				break;
+				
+				case 2: 
+				cout<<"Cambiar registro de Oviparos\n";
+				break;
+				
+				case 3:
+				cout<<"Volviendo al menu principal\n";
+				break;
+				}
+			break;
+			
+			case 4:
+			cout<<"Encuentra a tu mascota ideal \n\n";
+			break;
+			
+			case 5:
+			cout<<"Gracias\n";
+			break;
+		break;
+		}
+		
+	}while (opcion != 5);
 }
+	
